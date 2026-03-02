2b46bde0dea312136e0a414a9e86aaa0f1609434
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2026-03-02 20:17:38 -0300

    Commit inicial: Estructura de carpetas

---
+++ Unidad 1 - Intro Web/Introducción a JavaScript/script.js
@@ -0,0 +1,6 @@
+function login() {
+    const user = document.getElementById("txtUser").value;
+    const pass = document.getElementById("txtPass").value;
+
+ console.log("Hola " + user + ", tu contraseña es: " + pass);
+}
