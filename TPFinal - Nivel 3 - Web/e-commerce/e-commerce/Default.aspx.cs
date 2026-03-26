52107d04eb415b81dade131c497b213a30d021a6
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2026-03-26 12:57:57 -0300

    feat: implementar pantalla de detalles y ajustar layout con Flexbox. - Creación de Detalle.aspx para visualizar la información completa y ampliada de cada artículo. - Implementación de navegación por URL y recepción de datos mediante Request.QueryString["id"]. - Uso de expresiones Lambda (.Find) en el Code-Behind para filtrar el artículo seleccionado desde la capa de Negocio. - Manejo de excepciones visuales (artículo no encontrado o ID nulo) renderizando alertas de Bootstrap. - Refactorización de MasterPage.Master integrando clases de Flexbox (d-flex, flex-column, min-vh-100, mt-auto) para forzar el anclaje del Footer al final del viewport sin importar la cantidad de contenido.

--- TPFinal - Nivel 3 - Web/e-commerce/e-commerce/Default.aspx.cs
+++ TPFinal - Nivel 3 - Web/e-commerce/e-commerce/Default.aspx.cs
@@ -11,17 +11,16 @@ namespace e_commerce
 {
     public partial class Default : System.Web.UI.Page
     {
-        //Propiedad pública para que el Front-End (Html) pueda leer la lista
         public List<Articulo> ListaArticulos { get; set; }
-
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Instancio el negocio que traje desde el nivel 2
             ArticuloNegocio negocio = new ArticuloNegocio();
 
-            //Lleno la lista con el método listar()
+           
             ListaArticulos = negocio.Listar();
+
         }
-    }
+
+        }
 }
---
+++ TPFinal - Nivel 3 - Web/e-commerce/e-commerce/Detalle.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace e_commerce
+{
+    public partial class Detalle : System.Web.UI.Page
+
+    {
+        //  Propiedad pública para guardar el artículo  y que el html lo pueda leer
+        public Articulo ArticuloSeleccionado {  get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"] != null)
+            {
+                int id = int.Parse(Request.QueryString["id"]);
+
+                ArticuloNegocio negocio = new ArticuloNegocio();
+
+                ArticuloSeleccionado = negocio.Listar().Find(x=>x.Id == id);
+
+                
+            }
+        }
+    }
+}
