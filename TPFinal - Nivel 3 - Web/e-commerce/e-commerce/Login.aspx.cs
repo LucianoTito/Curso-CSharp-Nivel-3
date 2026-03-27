05e90f478e4ab2f76a3109c4f87459e39071d2a7
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2026-03-27 01:21:51 -0300

    feat: lógica de validación de Login contra la base de datos

--- TPFinal - Nivel 3 - Web/e-commerce/e-commerce/Login.aspx.cs
+++ TPFinal - Nivel 3 - Web/e-commerce/e-commerce/Login.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace e_commerce
 {
@@ -16,7 +18,49 @@ namespace e_commerce
 
         protected void btnIngresar_Click (object sender, EventArgs e)
         {
+            if (Session["usuario"] != null)
+            {
+                Response.Redirect("Default.aspx", false);
+            }
 
+
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtEmail.Text) || string.IsNullOrEmpty(txtPassword.Text))
+                {
+                    lblError.Text = "Por favor, complete ambos campos para ingresar.";
+                    lblError.Visible = true;
+                    return;
+                }
+
+                Usuario user = new Usuario();
+                UsuarioNegocio negocio = new UsuarioNegocio();
+
+                user.Email = txtEmail.Text; 
+                user.Pass = txtPassword.Text;
+
+                if (negocio.Loguear(user))
+                    {
+                    //Si es true
+                    Session.Add("usuario", user);
+                      }
+                else
+                {
+                    lblError.Text = "Email o contraseñas incorrectos. Intente nuevamente";
+                    lblError.Visible= true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                lblError.Text = "Ocurrió un error inesperado: " + ex.Message;
+                lblError.Visible = true;
+            }
         }
     }
 }
